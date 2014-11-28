f11239a1ebfa33ba0d67307c3e5f203f07214965
Stefan Herrmann <stefan.hrrmnn@gmail.com>
2014-11-28 15:54:46 +0100

    [not working] game mode structure

---
+++ Reciever/js/init.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Stefan on 28.11.2014.
+ */
+function initialize() {
+
+    window.map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: new google.maps.LatLng(45.74167213456433, 38.26884827734375),
+        zoom: 3,
+        mapTypeControl: true,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+
+    });
+
+}
