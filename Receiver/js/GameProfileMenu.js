ee4eb6c3db58387aae81b463e25e51cd4083b000
Martin Haut <martin.haut@arcor.de>
2014-12-11 19:15:03 +0100

    working at menus...

---
+++ Receiver/js/GameProfileMenu.js
@@ -0,0 +1,12 @@
+(function(castReceiver){
+
+    castReceiver.init = function(){
+        console.log('running gameProfileMenu.init');
+        $('#gameOverlay').load('templates/GameProfileMenu.html', function (data) {
+            $(this).html(data);
+        });
+
+        console.log('gameProfileMenu initialized');
+    }
+
+}(this.mainMenu = this.mainMenu || {}));
