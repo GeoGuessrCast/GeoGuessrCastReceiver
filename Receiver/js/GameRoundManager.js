be084af21cfc758c33457d8bc12830511e8436ff
Martin Haut <martin.haut@arcor.de>
2015-01-23 00:05:44 +0100

    fixed fatal error in calculateAnswer (geoCoder) function

--- Receiver/js/GameRoundManager.js
+++ Receiver/js/GameRoundManager.js
@@ -29,8 +29,7 @@
 
     grm.startRound = function(){
         gameRoundManager.currentGameState = data.gameState.guessing;
-        console.log("\n======= Round " + gameModeManager.currentRound + " =======");
-        displayText("\n======= Round " + gameModeManager.currentRound + " =======");
+        print("\n======= Round " + gameModeManager.currentRound + " =======");
 
         //var countryCode = dataManager.getRandomCountryCode();
         var userList = userManager.getUserList();
@@ -55,7 +54,7 @@
 
         var geoNameChoices = dataManager.getCityNameArray(geoObjects);
         console.debug('[geoObjects] ' + geoObjects);
-        console.log('[geoNameChoices] ' + geoNameChoices);
+        print('[geoNameChoices] ' + geoNameChoices);
 
 
         var jsonData = {"event_type": data.eventType.startGame,
@@ -74,7 +73,7 @@
 
     grm.choseAnswer = function(userMac, answer){
         var user = userManager.getUserByMac(userMac);
-        console.log("[GRM] " + user.name + " picked " + answer);
+        print("[GRM] " + user.name + " picked " + answer);
         if (gameModeManager.currentGameModeProfile.multipleChoiceMode) {
             _calculateAnswer(user, answer);
         } else {
@@ -86,14 +85,13 @@
 
     grm.endRound = function(){
         gameRoundManager.currentGameState = data.gameState.evaluating;
-        displayText('Round ' + gameModeManager.currentRound +  ' ended.<br>' );
+        print('-> Round ' + gameModeManager.currentRound +  ' ended.' );
         renderManager.showMidScreenMessage('Answer: ' + gameRoundManager.goalGeoObject.name, gameRoundManager.roundEvaluationTimeSec-3 );
         var userList = userManager.getUserList();
         for(var i = 0; i < userList.length; i++){
             var user = userList[i];
             if (user.lastAnswerGiven != null) {
                 user.pointsInCurrentGame += user.lastAnswerGiven.points;
-                displayText("[GRM] " + user.name + " got " + user.lastAnswerGiven.points + " points (for "+user.lastAnswerGiven.distanceToGoalKm+" km)");
             }
         }
         renderManager.refreshBottomScoreboard();
@@ -107,11 +105,11 @@
         gameRoundManager.currentGameState = data.gameState.ended;
         if (gameRoundManager.roundTimer != null) {
             gameRoundManager.roundTimer.terminate();
-            console.log('[GRM] killed roundTimer: ' + gameRoundManager.roundTimer);
+            print('[GRM] killed roundTimer: ' + gameRoundManager.roundTimer);
         }
         if (gameRoundManager.roundTimerAnim != null) {
             gameRoundManager.roundTimerAnim.terminate();
-            console.log('[GRM] killed roundTimerAnim: ' + gameRoundManager.roundTimerAnim);
+            print('[GRM] killed roundTimerAnim: ' + gameRoundManager.roundTimerAnim);
         }
     };
 
@@ -121,7 +119,7 @@
         // check if max rounds reached
         if(gameModeManager.currentRound === gameModeManager.maxRounds) {
             // end game mode
-            displayText('[GRM] maxRounds reached: ' + gameModeManager.maxRounds);
+            print('[GRM] maxRounds reached: ' + gameModeManager.maxRounds);
             var jsonData = {"ended": true, "event_type":"game_ended"};
             eventManager.broadcast(data.channelName.game, jsonData);
             executionManager.execDelayed(gameRoundManager.roundEvaluationTimeSec*1000, renderManager.loadMainMenu);
@@ -146,10 +144,10 @@
         var points;
         if (answer == gameRoundManager.goalGeoObject.name) {
             points = data.constants.maxPointsPerAnswer;
-            console.log("[GRM] " + user.name + " got " + points + " points for the RIGTH answer.");
+            print("[GRM] " + user.name + " got " + points + " points for the RIGTH answer.");
         } else {
             points = 0;
-            console.log("[GRM] " + user.name + " got " + points + " points for the WRONG answer.");
+            print("[GRM] " + user.name + " got " + points + " points for the WRONG answer.");
         }
         user.lastAnswerGiven = new grm.Answer(answer,0,null,points);
         renderManager.refreshBottomScoreboard();
@@ -167,19 +165,28 @@
         }, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 var pos = results[0].geometry.location;
-                var countryCode = results[0].address_components[2].short_name;
+                //console.debug(results[0]);
+                var countryCode = '';
+                for (var i=0; i<results[0].address_components.length; i++) {
+                    for (var b=0;b<results[0].address_components[i].types.length;b++) {
+                        if (results[0].address_components[i].types[b] == "country") {
+                            countryCode = results[0].address_components[i];
+                            break;
+                        }
+                    }
+                }
                 // get Distance to right answer (if not the same)
                 var distInKm = _getDistance(pos,gameRoundManager.goalGeoObject.position)/1000;
                 // Create new GeoObject for given answer
                 var guessedGeoObject = new dataManager.GeoObject(user.mac,answer,pos.k,pos.B,countryCode,0,0,null);
                 // Create new Answer Object
                 var points = Math.floor(Math.max(0,Math.min(data.constants.maxPointsPerAnswer,(data.constants.maxDistanceErrorKm+100-distInKm)/100)));
+                distInKm = Math.floor(distInKm);
                 user.lastAnswerGiven = new grm.Answer(answer,distInKm,guessedGeoObject,points);
                 renderManager.refreshBottomScoreboard();
-                console.log("[GRM] " + user.name + " got " + points + " points (for "+distInKm+" km)");
+                print("[GRM] " + user.name + " got " + points + " points (for "+distInKm+" km)");
             } else {
-                console.log(userMac+ ' Address could not be geocoded: ' + status);
-                displayText(userMac+ ' Address could not be geocoded: '+answer+" : " + status);
+                print(user.mac+ ' Address could not be geocoded: '+answer+" : " + status);
 
             }
         });
