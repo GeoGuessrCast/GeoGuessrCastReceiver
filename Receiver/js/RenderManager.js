905bf17e670f4241390d554aca21a685dc2656b0
Martin Haut <martin.haut@arcor.de>
2015-01-23 16:21:26 +0100

    deploy test (icon)

--- Receiver/js/RenderManager.js
+++ Receiver/js/RenderManager.js
@@ -146,7 +146,7 @@
             var userAnswer = user.lastAnswerGiven;
             if (isGuessingState) {
                 if (userAnswer == null) {
-                    iconCssClass = 'waitingIcon'; //TODO
+                    iconCssClass = 'mountainIcon'; //TODO
                 } else {
                     iconCssClass = 'readyIcon';
                 }
@@ -169,7 +169,7 @@
                 + '</span></span>');
 
             var userStateDiv = $('#userState_'+user.mac);
-            userStateDiv.removeClass('waitingIcon');
+            userStateDiv.removeClass('mountainIcon');
             userStateDiv.removeClass('readyIcon');
             userStateDiv.removeClass('noIcon');
             userStateDiv.addClass(iconCssClass);
