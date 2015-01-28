e85cdd4c7936e28f00f77ebb8cec9d4fde610941
Martin Haut <martin.haut@arcor.de>
2015-01-28 17:41:47 +0100

    QUERY TESTER :  _query_tester.js    TYPE@CONSOLE:   queryTest()

--- Receiver/js/_query_tester.js
+++ Receiver/js/_query_tester.js
@@ -6,11 +6,17 @@ function sqlTest() {
     var googleApiKey = "AIzaSyBDXF2p6in0gxcCMZVepVyvVHy_ASfmiXo";
 
     var queryStr = [];
-    queryStr.push("SELECT * ");
+    queryStr.push("SELECT countryCode, COUNT() as numberOfCities, SUM(population) AS populationSum "); //ATTENTION:  MIN(latitude) AS minLat -> BAD_REQUEST (some lattidues are STRINGS  :( )
     queryStr.push(" FROM " + fusionTableId);
-    queryStr.push(" WHERE population>1000000");
+    queryStr.push(" WHERE countryCode='DE'");
+    queryStr.push(" GROUP BY countryCode");
 
-// queryStr.push(" WHERE ST_INTERSECTS(geometry, CIRCLE(LATLNG(37.4, -122.1), 500))");
+
+    //###EXAMPLES###:
+    //queryStr.push(" WHERE population>1000000");
+    //queryStr.push(" WHERE ST_INTERSECTS(geometry, CIRCLE(LATLNG(37.4, -122.1), 500))");
+
+
 
     var sql = encodeURIComponent(queryStr.join(" "));
     $.ajax({
